c1a2a1f03417435810ec35dbaeface3d9176a082
TemeTDK <temesgenfeseha@gmail.com>
2015-01-06 13:06:30 +0300

    Data Interface done

---
+++ AddressBook.API.DataInterface/IContactRepository.cs
@@ -0,0 +1,18 @@
+﻿using AddressBook.API.DataModel.Operations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBook.API.DataInterface
+{
+    public interface IContactRepository
+    {
+        ContactResponseDTO Save(ContactDTO request);
+        ContactResponseDTO Update(ContactDTO request);
+        ContactResponseDTO Delete(ContactDTO request);
+        ContactsResponseDTO GetContacts(ContactsDTO request);
+        ContactDetailResponseDTO GetDetail(ContactDetailDTO request);
+    }
+}
