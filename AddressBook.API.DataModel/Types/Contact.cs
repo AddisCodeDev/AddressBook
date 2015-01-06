e0d82c7461837b96468d6b54cb99bd895883410d
TemeTDK <temesgenfeseha@gmail.com>
2015-01-06 11:08:11 +0300

    Contact Model created

---
+++ AddressBook.API.DataModel/Types/Contact.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBook.API.DataModel.Types
+{
+    public class Contact
+    {
+        public Guid Id { get; set; }
+        public String FirstName { get; set; }
+        public String LastName { get; set; }
+        public String Email { get; set; }
+        public String PhoneNumber { get; set; }
+        public String PictureURL { get; set; }
+    }
+}
